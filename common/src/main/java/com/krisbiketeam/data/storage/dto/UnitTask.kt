1747083fde2314eceed54c8d2502cdc641a62acd
Krzysiek Płotczyk <krisbiketeam@gmail.com>
2018-07-20 23:30:32 +0200

    Finish generalize Storage Units

--- common/src/main/java/com/krisbiketeam/data/storage/dto/UnitTask.kt
+++ common/src/main/java/com/krisbiketeam/data/storage/dto/UnitTask.kt
@@ -3,6 +3,8 @@ package com.krisbiketeam.data.storage.dto
 data class UnitTask(var unitName: String = "",
                     var unitType: String = "",
                     var delay: Long = 0,
+                    var duration: Long = 0,
+                    var period: Long = 0,
                     var startTime: Long = 0,
                     var endTime: Long = 0,
                     var inverse: Boolean = false)
