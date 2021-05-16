0cf98161284ef39e2819c7ca98c0c5b5938fb2d5
krisbiketeam <krisbiketeam@gmail.com>
2021-05-16 07:36:48 +0200

    Set proper local polish TimeZone in things

--- things/src/main/java/com/krisbiketeam/smarthomeraspbpi3/ThingsApplication.kt
+++ things/src/main/java/com/krisbiketeam/smarthomeraspbpi3/ThingsApplication.kt
@@ -1,6 +1,7 @@
 package com.krisbiketeam.smarthomeraspbpi3
 
 import android.app.Application
+import com.google.android.things.device.TimeManager
 import com.google.firebase.FirebaseApp
 import com.krisbiketeam.smarthomeraspbpi3.di.myModule
 import com.krisbiketeam.smarthomeraspbpi3.utils.ConsoleLoggerTree
@@ -17,7 +18,12 @@ class ThingsApplication : Application() {
 
         // Set to ulimited number of corutines to run on new threads not limiter do default 64
         System.setProperty(IO_PARALLELISM_PROPERTY_NAME, Int.MAX_VALUE.toString())
+
+        // Set current RP3 Tings timezone
+        TimeManager.getInstance().setTimeZone("Europe/Warsaw")
+
         FirebaseApp.initializeApp(this)
+
         Timber.plant(ConsoleLoggerTree)
 
         startKoin {
